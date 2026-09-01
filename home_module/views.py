346b13445fe86150967f20e8ce65ce4c63bc7596
kohansal14 <khnsalmhmdhsyn35@gmail.com>
2026-09-01 22:35:42 +0330

    render_partial for header,footer
    render_partial for header,footer in files base.html

--- home_module/views.py
+++ home_module/views.py
@@ -6,3 +6,12 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request,'home_module/index.html')
+
+
+
+def site_header_component(request):
+    return render(request,'header_component.html')
+
+
+def site_footer_component(request):
+    return render(request,'footer_component.html')
